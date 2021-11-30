dbb28ba5e09e8a7698c6d44491f5c2944b849979
duange <duange@88.com>
2021-11-30 10:38:20 +0800

    v1.5

--- TcpServer1.5/server1.5.cpp
+++ TcpServer1.5/server1.5.cpp
@@ -8,7 +8,9 @@
 enum CMD
 {
 	CMD_LOGIN,
+	CMD_LOGIN_RESULT,
 	CMD_LOGOUT,
+	CMD_LOGOUT_RESULT,
 	CMD_ERROR
 };
 
@@ -19,25 +21,47 @@ struct DataHeader
 	short cmd;
 };
 
-struct Login
+struct Login:public DataHeader
 {
+	Login()
+	{
+		dataLength = sizeof(Login);
+		cmd = CMD_LOGIN;
+	}
 	char userName[32];
 	char PassWord[32];
 };
 
-struct LoginResult
+struct LoginResult :public DataHeader
 {
+	LoginResult()
+	{
+		dataLength = sizeof(LoginResult);
+		cmd = CMD_LOGIN_RESULT;
+		result = 0;
+	}
 	int result;
 
 };
 
-struct Logout
+struct Logout :public DataHeader
 {
+	Logout()
+	{
+		dataLength = sizeof(Logout);
+		cmd = CMD_LOGOUT;
+	}
 	char userName[32];
 };
 
-struct LogoutResult
+struct LogoutResult :public DataHeader
 {
+	LogoutResult()
+	{
+		dataLength = sizeof(LogoutResult);
+		cmd = CMD_LOGOUT_RESULT;
+		result = 0;
+	}
 	int result;
 };
 
@@ -95,28 +119,27 @@ int main()
 			printf("client quit\n");
 			break;
 		}
-		printf("access command :%s length: %hd \n", header.cmd, header.dataLength);
+		
 		switch (header.cmd)
 		{
 		case CMD_LOGIN:
 		{
 			Login login = {};
-			recv(_cSock, (char*)&login, sizeof(Login), 0);
+			//login数据在前面已经被读取过了，故而需要偏移
+			recv(_cSock, (char*)&login+sizeof(DataHeader), sizeof(Login)- sizeof(DataHeader), 0);
+			printf("access CMD_LOGIN length: %d , UserName: %s ,Passwd : %s\n", login.dataLength,login.userName,login.PassWord);
 			//判断用户名密码是否正确的过程忽略
-			LoginResult ret = { 0 };
-
-			send(_cSock, (char*)&header, sizeof(DataHeader), 0);
+			LoginResult ret ;
 			send(_cSock, (char*)&ret, sizeof(LoginResult), 0);
 		}
 		break;
 		case CMD_LOGOUT:
 		{
 			Logout loginout = {};
-			recv(_cSock, (char*)&loginout, sizeof(Logout), 0);
+			recv(_cSock, (char*)&loginout + sizeof(DataHeader), sizeof(Login) - sizeof(DataHeader), 0);
+			printf("access CMD_LOGOUT length: %d , UserName: %s \n", loginout.dataLength, loginout.userName);
 			//判断用户名密码是否正确的过程忽略
-			LogoutResult ret = { 0 };
-
-			send(_cSock, (char*)&header, sizeof(DataHeader), 0);
+			LogoutResult ret;
 			send(_cSock, (char*)&ret, sizeof(LogoutResult), 0);
 		}
 		break;
